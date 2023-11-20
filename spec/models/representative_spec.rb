244b84e566f59e68cefdae240b52df13e02d3c82
David Chung <dachung@berkeley.edu>
2023-11-20 00:30:18 +0000

    Fixed test case

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -4,8 +4,28 @@ require 'rails_helper'
 
 describe Representative do
   describe 'civic_api_to_representative_params' do
-    let!(:existing_rep) { described_class.create!(name: 'Taylor Swift', ocdid: '123', title: 'Senator') }
-    let(:official) { OpenStruct.new(name: 'Taylor Swift') }
+    let!(:existing_rep) do
+      described_class.create!(
+        name:      'Taylor Swift',
+        ocdid:     '123',
+        title:     'Senator',
+        street:    '1021 O Street',
+        city:      'Sacramento',
+        state:     'CA',
+        zip:       '95814',
+        party:     'Democrat',
+        photo_url: 'http://example.com/taylor_swift.jpg'
+      )
+    end
+    let(:address) { OpenStruct.new(line1: '1021 O Street', city: 'Sacramento', state: 'CA', zip: '95814') }
+    let(:official) do
+      OpenStruct.new(
+        name:      'Taylor Swift',
+        address:   [address],
+        party:     'Democrat',
+        photo_url: 'http://example.com/taylor_swift.jpg'
+      )
+    end
     let(:office) { OpenStruct.new(name: 'Senator', division_id: '123', official_indices: [0]) }
     let(:rep_info) { OpenStruct.new(officials: [official], offices: [office]) }
 
@@ -15,6 +35,8 @@ describe Representative do
 
     it 'does not add an additional record for an existing representative' do
       described_class.civic_api_to_representative_params(rep_info)
+      puts described_class.all.inspect
+
       expect(described_class.count).to eq(1)
       expect(described_class.last).to eq(existing_rep)
     end
